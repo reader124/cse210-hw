9f7a1be05b6fffabf50db93c69957e46da387992
reader124 <sereading9999@gmail.com>
2023-09-16 10:02:47 -0600

    finished prep2 program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        bool srPassing = true;
+        string srLetterGrade = "";
+
+        Console.WriteLine("Please enter your grade: ");
+        string srInputtedGrade = Console.ReadLine();
+        int srGrade = int.Parse(srInputtedGrade);
+
+        if (srGrade >= 90) { srLetterGrade = "A"; }
+        else if (srGrade >= 80) { srLetterGrade = "B"; }
+        else if (srGrade >= 70) { srLetterGrade = "C"; }
+        else if (srGrade >= 60) { srLetterGrade = "D"; srPassing = false; }
+        else { srLetterGrade = "F"; srPassing = false; }
+
+        Console.WriteLine($"Your grade is {srLetterGrade}.");
+
+        if (srPassing) { Console.WriteLine("Well Done!"); }
+        else { Console.WriteLine("Better luck next time!"); }
     }
 }
