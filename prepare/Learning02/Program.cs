6aa4ebcfe6e6bb84c708862fdb3e61b05064bbe4
reader124 <sereading9999@gmail.com>
2023-09-25 15:50:09 -0600

    finished learning02 program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job srJobOne = new Job();
+        srJobOne._company = "Nintendo";
+        srJobOne._jobTitle = "Developer";
+        srJobOne._startYear = 2000;
+        srJobOne._endYear = 2010;
+
+        Job srJobTwo = new Job();
+        srJobTwo._company = "Mojang";
+        srJobTwo._jobTitle = "QA Tester";
+        srJobTwo._startYear = 2020;
+        srJobTwo._endYear = 2024;
+
+        Resume srMyResume = new Resume();
+        srMyResume._name = "Sean Reading";
+        srMyResume._jobs.Add(srJobOne);
+        srMyResume._jobs.Add(srJobTwo);
+
+        srMyResume.Display();
     }
 }
